348e158c153a06dc465f248e57ed92ea835ae5be
visheshdvn <vishesh.dh1@gmail.com>
2021-04-20 02:35:13 +0530

    auth form added in signup screen

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,17 +1,38 @@
 import React from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
     return (
-        <>
-            <Text style={{ fontSize: 48 }}>sup Screen</Text>
-            <Button
-                title="Go to Signin"
-                onPress={() => navigation.navigate("mainFlow")}
-            />
-        </>
+        <View style={styles.container}>
+            <Spacer>
+                <Text h3>Sign Up for Tracker</Text>
+            </Spacer>
+            <Input label="Email" />
+            <Spacer />
+            <Input label="Password" />
+            <Spacer>
+                <Button title="Sign Up" />
+            </Spacer>
+        </View>
     );
 };
+
+SignupScreen.navigationOptions = () => {
+    return {
+        header: () => false,
+    };
+};
+
+const styles = StyleSheet.create({
+    container: {
+        // flex: 1,
+        height: "100%",
+        justifyContent: "center",
+        // marginBottom: 250,
+    },
+});
 
 StyleSheet.create({});
 
