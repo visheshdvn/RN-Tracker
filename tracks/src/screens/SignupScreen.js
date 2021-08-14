0f4a4f29c1552949f6141f89c779fc93d651ac3d
visheshdvn <vishesh.dh1@gmail.com>
2021-08-14 21:25:47 +0530

    AuthForm and NavLink components created

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,45 +1,24 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { View, StyleSheet } from "react-native";
-import { Text, Input, Button } from "react-native-elements";
-import Spacer from "../components/Spacer";
 import { Context as AuthContext } from "../context/AuthContext";
+import AuthForm from "../components/AuthForm";
+import NavLink from "../components/NavLink";
 
 const SignupScreen = ({ navigation }) => {
     const { state, signup } = useContext(AuthContext);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    console.log(state);
 
     return (
         <View style={styles.container}>
-            <Spacer>
-                <Text h3>Sign Up for Tracker</Text>
-            </Spacer>
-            <Input
-                label="Email"
-                value={email}
-                onChangeText={setEmail}
-                autoCapitalize="none"
-                autoCorrect={false}
+            <AuthForm
+                headerText="Sign Up for tracker"
+                errorMessage={state.errorMessage}
+                submitButtonText="Sign Up"
+                onSubmit={signup}
             />
-            <Spacer />
-            <Input
-                secureTextEntry
-                label="Password"
-                value={password}
-                onChangeText={setPassword}
-                autoCapitalize="none"
-                autoCorrect={false}
-            />
-            {state.errorMessage ? (
-                <Text style={styles.errorMessage}>{state.errorMessage}</Text>
-            ) : null}
-            <Spacer>
-                <Button
-                    title="Sign Up"
-                    onPress={() => signup({ email, password })}
-                />
-            </Spacer>
+
+            <NavLink
+            routeName="Signin"
+            text="Already have an account? Sign in instead!" />
         </View>
     );
 };
