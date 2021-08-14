5c4e16d1bcac4e3f897adc14d008545efcc1a1e6
visheshdvn <vishesh.dh1@gmail.com>
2021-08-15 03:02:58 +0530

    signout feature added

---
+++ tracks/src/components/Map.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const Map = () => {
+    return <Text>I am a Map!</Text>;
+};
+
+const styles = StyleSheet.create({
+
+})
+
+export default Map;
--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -12,6 +12,8 @@ const authReducer = (state, action) => {
             return { errorMessage: "", token: action.payload };
         case "clear_error_message":
             return { ...state, errorMessage: "" };
+        case "signout":
+            return { token: null, errorMessage: "" };
         default:
             return state;
     }
@@ -72,10 +74,10 @@ const signin =
         }
     };
 
-const signout = (dispatch) => {
-    return () => {
-        // somehow sign out!!!
-    };
+const signout = (dispatch) => async () => {
+    await AsyncStorage.removeItem("token");
+    dispatch({ type: "signout" });
+    navigate("loginFlow");
 };
 
 export const { Provider, Context } = createDataContext(
